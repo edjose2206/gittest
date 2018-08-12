c222c54e8019def373cce706369329e1e490449f
edjose2206 <edjose182@gmail.com>
2018-08-12 15:18:06 -0600

    Se agregaro una linea de codigo para ver como responde el git

--- reverse.py
+++ reverse.py
@@ -5,3 +5,5 @@ a=np.array([0,1,2,3,4,5])  ##Arreglo de numeros
 b= a[::-1] ##b va del primer valor del arreglo hasta el final
 
 print b ##Imprime el valor de b
+
+##Veamos que pasa al agregar estas lineas de código
