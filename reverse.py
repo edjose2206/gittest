b275275323f3068ea94f35b492b160fd36d289cd
edjose2206 <edjose182@gmail.com>
2018-08-12 15:16:57 -0600

    Este es un archivo python con un arreglo de datos

---
+++ reverse.py
@@ -0,0 +1,7 @@
+import numpy as np ##biblioteca de python
+
+a=np.array([0,1,2,3,4,5])  ##Arreglo de numeros
+
+b= a[::-1] ##b va del primer valor del arreglo hasta el final
+
+print b ##Imprime el valor de b
